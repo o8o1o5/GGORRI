40e8b4a1cc9e2225c2a3125c5917d0ac3f6d0133
노경열 <sikadeer105@gmail.com>
2025-05-27 02:21:10 +0900

    으어걱

---
+++ src/main/java/dev/o8o1o5/ggorri/GGORRICommand.java
@@ -0,0 +1,10 @@
+package dev.o8o1o5.ggorri;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
+
+public class GGORRICommand implements CommandExecutor, TabCompleter {
+    private final GGORRI plugin;
+
+    public GGORRICommand
+}
---
+++ src/main/java/dev/o8o1o5/ggorri/game/PlayerGameData.java
@@ -0,0 +1,4 @@
+package dev.o8o1o5.ggorri.game;
+
+public class PlayerGameData {
+}
